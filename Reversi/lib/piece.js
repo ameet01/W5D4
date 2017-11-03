67d16330556d1d62179f2be3134da41fd7fef061
Ameet Vadhia <ameet01@gmail.com>
2017-11-02 17:54:33 -0700

    reversi

---
+++ Reversi/lib/piece.js
@@ -0,0 +1,33 @@
+/**
+ * Initializes the Piece with its color.
+ */
+function Piece (color) {
+  this.color = color;
+}
+
+/**
+ * Returns the color opposite the current piece.
+ */
+Piece.prototype.oppColor = function () {
+  if(this.color === 'white') {
+    return 'black';
+  } else {
+    return 'white';
+  }
+};
+
+/**
+ * Changes the piece's color to the opposite color.
+ */
+Piece.prototype.flip = function () {
+  this.color === 'white' ? this.color = 'black' : this.color = 'white';
+};
+
+/**
+ * Returns a string representation of the string
+ * based on its color.
+ */
+Piece.prototype.toString = function () {
+};
+
+module.exports = Piece;
